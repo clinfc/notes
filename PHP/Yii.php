7ad4fbeec587607b34c3fcf4a4513b62a10ae5ef
clin <39868400+clinfc@users.noreply.github.com>
2019-11-27 21:26:49 +0800

    Update Yii.php

--- PHP/Yii.php
+++ PHP/Yii.php
@@ -1,1 +1,15 @@
 <?php
+
+/**
+ * 路由配置
+ * config/web.php => urlManager
+ * 
+ * 错误处理
+ * config/web.php => errorHandler => errorAction
+ * 
+ * 持续迭代（CI）之版本号实现
+ *  版本号作用：
+ *    1、记录系统的成长迭代
+ *    2、可以方便错误回滚
+ *    3、解决静态文件缓存
+ */
