227f94cf223304d13b82d6954c69adbaaf461a1b
clin <39868400+clinfc@users.noreply.github.com>
2019-12-27 21:43:29 +0800

    Update ES5 - ES10.js

--- JS/ES5 - ES10.js
+++ JS/ES5 - ES10.js
@@ -80,6 +80,10 @@
 				return 
 			})
 
+		ES7：
+
+			array.includes(val)					返回 true 或 false
+
  */
 
  /*
@@ -319,6 +323,8 @@
 
 				promise.catch(err => {})							实例方法。统一错误处理（链式操作中处理多个 then 的错误）。catch 只能捕获 reject 触发的错误，无法捕获 throw
 
+				promise.finally(() => {})							ES9：类似于 try...catch...finally...
+
 				
 			
 			串行案例：按顺序加载 1.js、2.js、3.js
@@ -351,6 +357,79 @@
 				}).catch(err => {
 	
 				})
+
+
+		ES8：（async 和 await 为 Promise 的语法糖）
+
+			async function asyncTest() {							定义
+				return '渣渣辉'										自定义返回（返回的非 Promise 对象时，自动转换为 Promise.resolve('渣渣辉') ）
+			}
+			let at = asyncTest()									返回 Promise 对象
+			at.then(value => {										接收自定义返回（value 的值为 “渣渣辉”）
+				
+			})
+
+
+			async function asyncTest() {
+				let promise = new Promise((resolve, reject) => {})
+
+				await promise 										在异步函数中执行异步函数并确保执行顺序（await 将自动处理为 Promise 对象）
+
+				console.log('')
+			}
+
+
+		ES9：（for await of）遍历异步操作集合
+
+			function gen(time) {
+				return new Promise((resolve, reject) => {
+					setTimeout(function () {
+						resolve(time)
+					}, time)
+				})
+			}
+			async function test () {
+				let array = [gen(100), gen(200), gen(300)]
+
+				for await (let item of array) {						for await of
+					console.log(Date.now(), item)
+				}
+			}
+			test()
+
+
+			let object = {
+				count: 0,
+				gen (time) {
+					return new Promise((resolve, reject) => {
+						setTimeout(function () {
+							resolve({dome: false, value: time})
+						}, time)
+					})
+				},
+				[Symbol.asyncIterator] () {
+					let self = this
+					return {
+						next () {
+							self.count++
+							if (self.count < 4) {
+								return self.gen(Math.random() * 1000)
+							} else {
+								return Promise.resolve({
+									done: true,
+									value: ''
+								})
+							}
+						}
+					}
+				}
+			}
+			async function test () {
+				for await (let item of object) {						for await of
+					console.log(Date.now(), item)
+				}
+			}
+			test()
  */
 
  /*
@@ -420,7 +499,7 @@
  */
 
  /*
-	暂停遍历：
+	暂停遍历（generator）：
 
 		ES6：
 
@@ -432,4 +511,213 @@
 			let l = loop()							此时不会输出
 			l.next()								此时输出“0”。调用一次“next”，输出一次。类似于“下一步”
 			l.next()								此时输出“1”
- */ 
+			l.return()								终止循环
+			l.throw(new Error('error'))				抛入异常（内部通过 try...catch... 捕获异常）
+
+			l.next(10)								设置值
+			l.return(10)							设置值
+
+	数据结构“可遍历”化（自定义遍历）（iterator）：
+
+		let obj = {
+			a: {
+				a1: [],
+				a2: [],
+				a3: []
+			},
+			b: []
+		}
+
+		ES5：
+
+			let r = []
+			for (let [, v] of Object.entrices(obj.a)) {
+				r = r.concat(v)
+			}
+
+		ES6：
+
+			自定义遍历器：一：显示迭代器协议
+
+				obj[Symbol.iterator] = function () {
+					let all = this.a
+					let keys = Reflect.ownKeys(all)
+					let vals = []
+					return {							输出（返回一个对象）
+						next () {						输出对象中必须要有【next】方法
+							if (!vals.length) {
+								if (keys.length) {
+									vals = all[key[0]]
+									keys.shift()
+								}
+							}
+							return {					【next】方法返回固定的对象结构
+								done: !vals.length,
+								value: vals.shift()
+							}
+						}
+					}
+				}
+				let a = []
+				for (let v of obj) {
+					a.push(v.value)
+				}
+				console.log(a)							包含所有啊a下的 a1,a2,a3 值的集合
+
+			自定义遍历器：二：隐式迭代器协议
+
+				obj[Symbol.iterator] = function * () {
+					let all = this.a
+					let keys = Reflect.ownKeys(all)
+					let vals = []
+					while (true) {
+						if (!vals.length) {
+							if (keys.length) {
+								vals = all[keys[0]]
+								keys.shift()
+								yield vals.shift()
+							} else {
+								return false
+							}
+						} else {
+							yield vals.shift()
+						}
+					}
+				}
+				let a = []
+				for (let v of obj) {
+					a.push(v.value)
+				}
+				console.log(a)
+ */
+
+ /*
+	模块化开发：
+
+		ES5：require.js
+
+		ES6：
+
+			导出导入：一
+
+				export let name = '李白' 
+
+				import {name} from './***'					此处写导入文件的路径，但是需要省略掉 .js 后缀
+
+
+			导出导入：二：多变量导出
+
+				var name = '李白'
+				var age = 123
+				var sex = '男'
+				export {
+					name,
+					age,
+					sex
+				}
+
+				import {name, age, sex} from './***'
+
+
+			导出导入：三：定义默认导出项
+
+				var name = '李白'
+				var age = 123
+				var sex = '男'
+				export default name 					默认导出项（唯一的，只允许有一个）
+				export {
+					age,
+					sex
+				}
+
+				import name, {age, sex} from './***'
+				import name from './***'				只导入默认导出项（默认导出项在导入时可以自定义名字）
+
+
+			导出导入：四：自定义导入名
+
+				var name = '李白'
+				var age = 123
+				var sex = '男'
+				export default name 					默认导出项（唯一的，只允许有一个）
+				export {
+					age,
+					sex
+				}
+
+				import name, {age as ages} from './***'	自定义导入名
+
+
+			导出导入：五：导出对象
+
+				export default {			写法一
+					name: '李白',
+					age: 123
+				}
+
+				var data = {}				写法二
+				var desc = {}
+				export default {
+					data,
+					desc
+				}
+
+				import object from './***'
+
+
+			导出导入：六：导出类
+
+				class Test {}				写法一
+				export default Test
+
+				export class Test {}		写法二
+
+				export default class {}		写法三
+
+				import Test from './***'
+
+
+			导出导入：七：将所有的导入写入一个对象
+
+				import * as mo from './***'
+ */
+
+ /*
+	ES8：
+
+		Math.pow(2, 5) === 2 ** 5
+
+		Object.keys()
+
+		Object.values()
+
+		'1'.padStart(3, '0')				前补白（ 此处得到 001 ）
+
+		'1'.padEnd(3, '0')					后补白（ 此处得到 100 ）
+ */
+
+ /*
+	ES9：
+
+		spread：（深拷贝）
+
+			let input = {
+				a: 1, 
+				b: 2
+			}
+			let output = {
+				...input,					将 input 打散并填充到 output 中
+				c: 3
+			}
+
+		rest：
+
+			let input = {
+				a: 1, 
+				b: 2,
+				c: 3,
+				d: 4,
+				e: 5
+			}
+			let {a, b, ...rest} = input
+ */
