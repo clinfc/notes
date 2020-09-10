add2821eaa8227f7146000fc45f67ab77495375a
clinfc <1962109568@qq.com>
2020-09-10 10:43:30 +0800

    Update 常用正则.js

--- JS/常用正则.js
+++ JS/常用正则.js
@@ -16,3 +16,26 @@
 
 // 千分位。$&：表示匹配到的每一项的内容
 '1234567890'.replace(/\d{1,3}(?=(\d{3})+$)/g, '$&,')
+
+// 转小驼峰
+'one_two_three-four'.replace(/[_-]+[a-z]/g, function(m) {
+  return m[1].toUpperCase()
+})
+
+// 转大驼峰
+'one_two_three-four'.replace(/(^[a-z]|[_-]+[a-z])/g, function(t) {
+    return t[t.length - 1].toUpperCase()
+})
+
+// 匹配 HTML 中的某个 attribute
+`<table border="0"></table>`.match(/\<table[a-zA-Z0-9'"%=\s]+?(border="0")/)
+
+// 批量处理 HTML 中的某个 attribute
+str.replace(/\<table[a-zA-Z0-9'"%=\s]+?border="\d+"/ig, function(row) {
+    return row.replace(/border="(\d+)"/, function($0, $1) {
+        return $0.replace($1, 5)
+    })
+})
+str.replace(/\<table[a-zA-Z0-9'"%=\s]+?border="\d+"/ig, function(row) {
+    return row.replace(/(border=")\d+(")/, '$1n$2')
+})
